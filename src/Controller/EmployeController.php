c98403735e8f950e8c91e82e5b7669102966bb52
Thibault Roussel <thibault.roussel@gmx.com>
2025-01-06 11:06:39 +0100

    first commit

---
+++ src/Controller/EmployeController.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\EmployeRepository;
+use App\Form\EmployeType;
+use App\Form\RegisterType;
+use App\Entity\Employe;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Doctrine\ORM\EntityManagerInterface;
+
+class EmployeController extends AbstractController
+{
+    public function __construct(
+        private EmployeRepository $employeRepository,
+        private EntityManagerInterface $entityManager,
+    )
+    {
+
+    }
+
+    #[Route('/employes', name: 'app_employes')]
+    public function employes(): Response
+    {
+        $employes = $this->employeRepository->findAll();
+        
+        return $this->render('employe/liste.html.twig', [
+            'employes' => $employes,
+        ]);
+    }
+
+    #[Route('/employes/{id}', name: 'app_employe')]
+    public function employe($id): Response
+    {
+        $employe = $this->employeRepository->find($id);
+
+        if(!$employe) {
+            return $this->redirectToRoute('app_employes');
+        }
+        
+        return $this->render('employe/employe.html.twig', [
+            'employe' => $employe,
+        ]);
+    }
+
+    #[Route('/employes/{id}/supprimer', name: 'app_employe_delete')]
+    public function supprimerEmploye($id): Response
+    {
+        $employe = $this->employeRepository->find($id);
+
+        if(!$employe) {
+            return $this->redirectToRoute('app_employes');
+        }
+
+        $this->entityManager->remove($employe);
+        $this->entityManager->flush();
+        
+        return $this->redirectToRoute('app_employes');
+    }
+
+    #[Route('/employes/{id}/editer', name: 'app_employe_edit')]
+    public function editerEmploye($id, Request $request): Response
+    {
+        $employe = $this->employeRepository->find($id);
+
+        if(!$employe) {
+            return $this->redirectToRoute('app_employes');
+        }
+
+        $form = $this->createForm(EmployeType::class, $employe);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $this->entityManager->flush();
+            return $this->redirectToRoute('app_employes');
+        }
+
+        return $this->render('employe/employe.html.twig', [
+            'employe' => $employe,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    // ------------------REGISTER------------------
+
+    #[Route('/welcome', name: 'app_welcome')]
+    public function welcome(): Response
+    {
+        return $this->render('auth/welcome.html.twig');
+    }
+
+    #[Route('/register', name: 'app_register')]
+    public function register(Request $request, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $employe = new Employe();
+        $employe
+                ->setRoles(['ROLE_USER'])
+                ->setStatut('CDI')
+                ->setDateArrivee(new \DateTimeImmutable());
+
+        $form = $this->createForm(RegisterType::class, $employe);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $employe->setPassword($passwordHasher->hashPassword($employe, $form->get('password')->getData()));
+            
+            $this->entityManager->persist($employe);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('app_projets');
+        }
+
+        return $this->render('auth/register.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
